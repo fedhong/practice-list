d2e3da7f118ba8ce38f628a3186a419c4e7b10ec
hongliang <hongliang@meituan.com>
2020-11-24 10:58:17 +0800

    add initHash

--- hl-framework/src/framework/createRouter.js
+++ hl-framework/src/framework/createRouter.js
@@ -1,27 +1,26 @@
 const createRouter = (config) => {
-    Object.keys(config).forEach((key, i) => {
-        const container = document.getElementById(key);
-        container.innerHTML = config[key];
-        if (i > 0) {
-            container.style.display = 'none';
-        }
-    })
+
+    function initHash() {
+        let curKey = location.hash.replace('#', '');
+        curKey = curKey || 'index';
+
+        Object.keys(config).forEach((key, i) => {
+            const container = document.getElementById(key);
+            container.innerHTML = config[key];
+
+            if (curKey === key) {
+                container.style.display = '';
+            } else {
+                container.style.display = 'none';
+            }
+        })
+    }
 
     if (!window.onhashchange) {
-        window.onhashchange = function () {
-            let curKey = location.hash.replace('#', '');
-            curKey = curKey || 'index';
-
-            Object.keys(config).forEach((key, i) => {
-                const container = document.getElementById(key);
-                if (curKey === key) {
-                    container.style.display = '';
-                } else {
-                    container.style.display = 'none';
-                }
-            })
-        }
+        window.onhashchange = initHash;
     }
+
+    initHash();
 };
 
 export default createRouter
