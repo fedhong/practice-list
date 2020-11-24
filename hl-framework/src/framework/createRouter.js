48ca4924543b19654fe40310c2a5251dd83bbac7
hongliang <hongliang@meituan.com>
2020-11-24 11:01:28 +0800

    router innerHTML 初始化一次

--- hl-framework/src/framework/createRouter.js
+++ hl-framework/src/framework/createRouter.js
@@ -1,12 +1,16 @@
 const createRouter = (config) => {
 
+    Object.keys(config).forEach((key, i) => {
+        const container = document.getElementById(key);
+        container.innerHTML = config[key];
+    })
+
     function initHash() {
         let curKey = location.hash.replace('#', '');
         curKey = curKey || 'index';
 
         Object.keys(config).forEach((key, i) => {
             const container = document.getElementById(key);
-            container.innerHTML = config[key];
 
             if (curKey === key) {
                 container.style.display = '';
