1faa2b11f46ce540ed67b7be4669bd32f70f2837
fedhong <www.hongliang@gmail.com>
2020-12-02 22:53:35 +0800

    move hl-framework to ../h5-framework

--- hl-framework/src/router/config.js
+++
@@ -1,9 +0,0 @@
-import Index from '../pages/index/script'
-import Profile from '../pages/profile/script'
-
-const RouterConfig = {
-    index: Index(),
-    profile: Profile(),
-};
-
-export default RouterConfig;
